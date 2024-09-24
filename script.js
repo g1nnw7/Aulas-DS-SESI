f931e827c6c5d135ad36a22faa54eb508ba10f04
Matheus Mathias dos Santos <numseiquenomecoloca@gmail.com>
2024-09-23 21:59:52 -0300

    Atividade html e Js 17/09

--- script.js
+++ script.js
@@ -1,91 +1,13 @@
-function ex1038(){
-    //ler o código
-    let codigo = Number(prompt("Digite o código:"));
-    //ler a quantidade
-    let quantidade = Number(prompt("Quantidade:"))
-    //definir o preço do produto
-    let preco
-    if(codigo == 1){
-        preco = 4
-        let total
-        total = preco * quantidade
-        alert ("Ficou R$"+total)
-    }
-    else if(codigo == 2){
-        preco = 4.5
-        let total
-        total = preco * quantidade
-        alert ("Ficou R$"+total)
-    }
-    else if(codigo == 3){
-        preco = 5
-        let total
-        total = preco * quantidade
-        alert ("Ficou R$"+total)
-    }
-    else if(codigo == 4){
-        preco = 2
-        let total
-        total = preco * quantidade
-        alert ("Ficou R$"+total)
-    }
-    else if(codigo == 5){
-        preco = 1.5
-        let total
-        total = preco * quantidade
-        alert ("Ficou R$"+total)
-    }
-    else {
-        alert ("Ta vendo essa prr de número no cardápio? fdp🤬")
-    }
+function atv1(){
+    let n = Number(prompt("Digite um número!")); //Entrada manual
+    let i = 1;
+    while(i<=n){
+        alert(i);
+        i++;
     
+    }
 }
-function exddd(){
-    let ddd =  Number(prompt("Digite seu DDD:"))
-    if(ddd == 61){
-        alert ("Você é de Brasília!")
-    }
-    else if(ddd == 71){
-        alert ("Você é de Salvador!")
-    }
-    else if(ddd == 11){
-        alert ("Você é de São Paulo!")
-    }
-    else if(ddd == 21){
-        alert ("Você é do Rio de Janeiro!")
-    }
-    else if(ddd == 32){
-        alert ("Você é de Juiz de fora!")
-    }
-    else if(ddd == 19){
-        alert ("Você é de Campinas!")
-    }
-    else if(ddd == 27){
-        alert ("Você é de Juiz de Vitória!")
-    }
-    else if(ddd == 31){
-        alert ("Você é de Belo Horizonte!")
-    }
-    else{
-        alert ("Seu DDD não está cadastrado!")
-    }
-    }
-function ex1015(){
-  let x = Number(prompt("Digite o valor dp seu eixo X"));
-  let y = Number(prompt("Digite o valor do seu eixo y"));
-  if(x > 0 && y > 0){
-    alert ("Primeiro Quadrante!");
-  } 
-  else if (x > 0 && y < 0){
-    alert ("Segundo Quadrante!");
-  }
-  else if (x < 0 && y > 0){
-    alert ("Terceiro Quadrante!");
-  }
-  else if (x < 0 && y < 0){
-    alert ("Quarto Quadrante!");
-  }
-  else {
-    alert ("Quadrante não definido!")
-  }
+function atv2 (){
+    let n = Numer(prompt("Digite um número!"))
+    for(let i = 1 )
 }
