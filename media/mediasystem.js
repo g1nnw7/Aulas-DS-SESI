41d85803a5bde1f08cb912e9e752a39cacf41646
g1nnw7 <numseiquenomecoloca@gmail.com>
2024-10-10 22:01:51 -0300

    adicionadas divagações sobre arrayys e outras coisas

---
+++ media/mediasystem.js
@@ -0,0 +1,14 @@
+function volta(){
+    window.location.href = "index.html"
+}
+
+function calculaMedia(){
+    
+    let numero = Number(prompt ("Digite um número"));
+    let sequencia = 0;
+    while (sequencia <= numero){
+        alert(sequencia);
+        sequencia ++;
+
+    }
+}
---
+++ sortear 50 numeros/script.js
@@ -0,0 +1,11 @@
+let numeros = []; 
+
+function executar83(){
+    let numero; 
+
+    numero = Math.round(Math.random()*6)
+    
+    numeros.push(numero);
+    console.log(numeros);
+
+}
