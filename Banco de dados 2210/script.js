024978568790e9795fc8096b9a590c7fcd257534
g1nnw7 <numseiquenomecoloca@gmail.com>
2024-10-28 21:55:29 -0300

    Edição Banco de Dados

--- Banco de dados 2210/script.js
+++ Banco de dados 2210/script.js
@@ -1,9 +1,11 @@
 let bancoDeDados = [];
 function add(){
-    let usuario = document.getElementById("username").value;
-    let pos = bancoDeDados.indexOf(usuario.toUpperCase());
-    if( pos== -1){
-        bancoDeDados.push(usuario.toUpperCase());
+    let usuario = {
+    username:document.getElementById("username").value.toUpperCase(),
+    password:document.getElementById("senha").value
+    }
+    if( existe(usuario.username)== false){ 
+        bancoDeDados.push(usuario);
         alert ("Usuário cadastrado com sucesso!");
     }
     else{
@@ -11,40 +13,62 @@ function add(){
         
     }
     document.getElementById("username").value = null;
+    document.getElementById("senha").value = null;
 }
 function login(){
-    let usuario = document.getElementById("user").value;
-    let pos = bancoDeDados.indexOf(usuario.toUpperCase());
-    if(pos == -1){
-        alert("Usuário não cadastrado!");
+    let usuario = document.getElementById("userLogin").value.toUpperCase();
+    let senha = document.getElementById("pass").value;
+    if(autentica(usuario, senha) == false){
+        alert("Falha de autenticação");
     }
-    else{
-        alert("Bem vindo ao sistema!");  
-    }
-    document.getElementById("user").value = null;
+
+        else{
+            alert("Bem vindo ao sistema!");  
+        }
+
+    document.getElementById("userLogin").value = null;
+    document.getElementById("pass").value = null;
 }
 function remove(){
-    let usuario = document.getElementById("userDelete").value;
-    let pos = bancoDeDados.indexOf(usuario.toUpperCase());
-    if(pos == -1){
-        alert ("Usuário não encontrado");
+    let usuario = document.getElementById("userDeletar").value.toUpperCase();
+    let senha = document.getElementById("removePass").value;
+
+    if(autentica(usuario, senha) == false){
+        alert ("Usuário não encontrado!");
     }
-    else{
+    else {
         bancoDeDados.splice(pos, 1);
         alert("Usuário excluído com sucesso!");
+        }
+        document.getElementById("userDelete").value = null;
     }
-    document.getElementById("userDelete").value = null;
-}
 function edit(){
     let username = document.getElementById("userEdit").value;
-    let pos = bancoDeDados.indexOf(username.toUpperCase());
-    if(pos == -1){
+    
+    if(existe(usuario.userEdit)==false){
         alert("Usuário não encontrado no banco de dados!");
     }
     else{
-        let editavel = prompt("Digite o susbitituto:").toUpperCase();
-        bancoDeDados[pos] = editavel;
+        let novoValor = prompt("Digite o susbitituto:").toUpperCase();
+        bancoDeDados[pos] = novoValor;
         alert("Usuário alterado com sucesso");
     }
     document.getElementById("userEdit").value = null;
 }
+function existe(username){
+   for(let usuario of bancoDeDados){
+    if(usuario.username == username){
+        return true;
+    }
+}
+return false;
+}
+function autentica(username, password){
+    for(let usuario of bancoDeDados){
+        if(usuario.username == username && usuario.password == password){
+            return true;
+        }
+    }
+    return false
+}
+
